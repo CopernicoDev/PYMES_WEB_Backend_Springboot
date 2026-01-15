6832a62459d3cf57bcbe6cf2c1a72d9fbb616285
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-01-15 21:34:35 +0000

    Creando el backend

---
+++ src/main/java/com/pymes/backend/nicolas/pymes_web_backend_springboot/PymesWebBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pymes.backend.nicolas.pymes_web_backend_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PymesWebBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PymesWebBackendSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pymes/backend/nicolas/pymes_web_backend_springboot/PymesWebBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pymes.backend.nicolas.pymes_web_backend_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PymesWebBackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

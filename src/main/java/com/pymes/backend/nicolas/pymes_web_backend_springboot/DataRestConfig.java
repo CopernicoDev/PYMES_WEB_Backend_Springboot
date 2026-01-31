09dc10758dbdc60775255c7c0e0e838e43d89495
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-01-31 19:14:37 +0000

    Añadiendo controller y conectando a la base de datos

---
+++ src/main/java/com/pymes/backend/nicolas/pymes_web_backend_springboot/DataRestConfig.java
@@ -0,0 +1,9 @@
+package com.pymes.backend.nicolas.pymes_web_backend_springboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer{
+
+}

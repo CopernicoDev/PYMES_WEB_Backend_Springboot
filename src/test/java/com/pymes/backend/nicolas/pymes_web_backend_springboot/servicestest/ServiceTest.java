32b404518fe7165d620d4b54b5d725364b5c3366
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-02-03 20:05:28 +0000

    Creando la clase test para probar el service

---
+++ src/test/java/com/pymes/backend/nicolas/pymes_web_backend_springboot/servicestest/ServiceTest.java
@@ -0,0 +1,14 @@
+package com.pymes.backend.nicolas.pymes_web_backend_springboot.servicestest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.pymes.backend.nicolas.pymes_web_backend_springboot.repositories.UserRepository;
+import com.pymes.backend.nicolas.pymes_web_backend_springboot.services.UserService;
+
+public class ServiceTest {
+
+    UserRepository userRepository;
+
+    UserService userService;
+
+}

d448acb69cbb975b5b0930e4d6eefde1cf9593c3
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-02-08 20:44:31 +0000

    Solucionando problemas de conexión con la base de datos

--- src/main/java/com/pymes/backend/nicolas/pymes_web_backend_springboot/PymesWebBackendSpringbootApplication.java
+++ src/main/java/com/pymes/backend/nicolas/pymes_web_backend_springboot/PymesWebBackendSpringbootApplication.java
@@ -1,13 +1,17 @@
 package com.pymes.backend.nicolas.pymes_web_backend_springboot;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pymes.backend.nicolas.pymes_web_backend_springboot.models.User;
 import com.pymes.backend.nicolas.pymes_web_backend_springboot.repositories.UserRepository;
 
 @SpringBootApplication
-public class PymesWebBackendSpringbootApplication {
+public class PymesWebBackendSpringbootApplication implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
@@ -17,5 +21,12 @@ public class PymesWebBackendSpringbootApplication {
 
 
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<User> users = userRepository.findAllUsers();
+		users.stream().forEach(user -> System.out.println(user));
+	}
+
 
 }

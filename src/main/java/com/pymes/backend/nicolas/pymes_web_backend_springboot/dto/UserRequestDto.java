e0e2e7e8a00f6ecbcd407f57be2bb97d0b952f7e
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-05-16 10:12:28 +0100

    Creando DTOS para el objeto User

---
+++ src/main/java/com/pymes/backend/nicolas/pymes_web_backend_springboot/dto/UserRequestDto.java
@@ -0,0 +1,12 @@
+package com.pymes.backend.nicolas.pymes_web_backend_springboot.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserRequestDto(
+        @NotBlank String username,
+        @NotBlank @Email String email,
+        @NotBlank @Size(min = 6, max = 12) String password) {
+
+}
